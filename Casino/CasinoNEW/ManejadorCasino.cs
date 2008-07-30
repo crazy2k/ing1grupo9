f12468bbedc6d97cf062addd7512065e94e57946
pabloa <pabloa@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-07-30 15:19:55 +0000

    ManejadorCasino agregado.

---
+++ Casino/CasinoNEW/ManejadorCasino.cs
@@ -0,0 +1,22 @@
+// ManejadorCasino.cs created with MonoDevelop
+// User: pablo at 12:14 PM 7/30/2008
+//
+
+using System;
+
+namespace CasinoNEW
+{
+	
+	
+	public class ManejadorCasino
+	{
+		
+		public ManejadorCasino()
+		{
+		}
+		
+		public void entrarCasino(int id, string usuario, string modo) {
+			
+		}
+	}
+}
