ff80a33a971dcd83840664ab5de138ade0f1dc19
pabloa <pabloa@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-07-30 15:58:21 +0000

    Clase Configuracion agregada. Proyecto modificado.

---
+++ trunk/Casino/CasinoNEW/Configuracion.cs
@@ -0,0 +1,16 @@
+// Configuracion.cs created with MonoDevelop
+// User: pablo at 12:39 PM 7/30/2008
+//
+
+using System;
+
+namespace CasinoNEW
+{
+	
+	
+	public class Configuracion
+	{
+		public static readonly string ARCHIVO_USUARIOS = "usuarios.txt";
+	}
+		
+}
