db67b3b9001c4c6964aa4b2e8a841b60e61c7c1e
elmaildematiaz@gmail.com <elmaildematiaz@gmail.com@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-08-01 20:15:35 +0000

    * Casino.cs: Sólo merge de lo que había hecho marta con lo de pablo

--- Casino/CasinoNEW/Casino.cs
+++ Casino/CasinoNEW/Casino.cs
@@ -3,30 +3,154 @@
 //
 
 using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Collections.Generic;
 
-namespace CasinoNEW
+using Ficha = System.Int32; //Asi se hacen los renombres de tipo. 
+using Dinero = System.Decimal;
+
+namespace Casino
 {
-	
-	
-	public class Casino
-	{
-		
-		private static Casino instance = null;
-		
-		private bool estaAbierto = false;
-		
-		private Casino()
-		{
-		}
-		
-		public static Casino GetInstance() {
-			if (instance == null)
-				instance = new Casino();
-			return instance;
-		}
+    //Microsoft garantiza que esta implementacion de singleton es thread-safe.
+    //Para mas info ver http://msdn.microsoft.com/en-us/library/ms998558.aspx
+
+    public sealed class Casino
+    {
+        #region variables
+
+        private static volatile Casino instance;
+        private static object syncRoot = new Object();
+        private Dinero montoPozoFeliz;
+        private bool estaAbierto; //La inicializacion por default lo deja en false (cerrado)
+        private float probJugadaTodosPonen;
+        private float probJugadaFeliz;
+        private Dinero saldoActual;
+        private List<Ficha> fichas;
+        private float porcJugadaTodosPonen;
+
+        #endregion
+
+        #region getters
+        
+        public Dinero getMontoPozoFeliz()
+        {
+            return montoPozoFeliz;
+        }
+
+        public float getProbJugadaTodosPonen()
+        {
+            return probJugadaTodosPonen;
+        }
+
+        public float getProbJugadaFeliz()
+        {
+            return probJugadaFeliz;
+        }
+
+        public Dinero getSaldoActual()
+        {
+            return saldoActual;
+        }
+
+
+        #endregion
+
+        #region funciones
+
+        private Casino() {}
+        public static Casino getInstance()
+        {
+             if (instance == null) 
+             {
+                lock (syncRoot) 
+                {
+                   if (instance == null) 
+                      instance = new Casino();
+                }
+             }
+             return instance;
+        }
+        public TipoJugada obtenerTipoDeJugada()
+        {
+            Random r = new Random(DateTime.Now.Millisecond);
+            double rFloat = r.NextDouble();
+            TipoJugada tipoJug;
+            if (rFloat < probJugadaFeliz)
+            {
+                tipoJug = TipoJugada.Feliz;
+            }
+            else if (rFloat >= probJugadaFeliz & rFloat < probJugadaFeliz + probJugadaTodosPonen)
+            {
+                tipoJug = TipoJugada.TodosPonen;
+            }
+            else
+            {
+                tipoJug = TipoJugada.Normal;
+            }
+            return tipoJug;
+        }
+        public void pagar(Dinero costo, Jugador jug)
+        {
+            saldoActual -= costo;
+            jug.cobrar(costo);
+        }
+        public void cobrar(Dinero ganancia, Jugador jug)
+        {
+            saldoActual += ganancia;
+            jug.pagar(ganancia);
+        }
+        public bool estaAbierto()
+        {
+            return abierto;
+        }
+        public void abrir()
+        {
+            if (!abierto)
+            {
+                abierto = true;
+            }
+            else
+            {
+                throw new Exception("El casino ya estaba abierto.");
+            }
+        }
+        public void cerrar()
+        {
+            if (abierto)
+            {
+                GestionadorUsuarios gest = GestionadorUsuarios.getInstance();
+                List<Jugador> jug = gest.getJugadoresActivos();
+                if (jug.Count == 0)
+                {
+                    abierto = false;
+                }
+                else
+                {
+                    throw new Exception("Hay jugadores activos en el casino.");
+                }
+            }
+            else
+            {
+                throw new Exception("El casino ya estaba cerrado.");
+            }
+        }
 		
 		public bool EstaAbierto() {
 			return estaAbierto;
 		}
-	}
+		
+        public void configurar()
+        {
+        }
+
+        #endregion
+    }
 }
+
