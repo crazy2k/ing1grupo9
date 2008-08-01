4137ab9f73c4fbe8fd3c6767814d452c1ed7b9ef
elmaildematiaz@gmail.com <elmaildematiaz@gmail.com@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-08-01 21:22:42 +0000

    * Juego.cs: Le metí un cerrar mesa * JuegoDados.cs: creo que está todo

--- trunk/Casino/CasinoNEW/JuegoDados.cs
+++ trunk/Casino/CasinoNEW/JuegoDados.cs
@@ -5,5 +5,20 @@ namespace CasinoNEW
 {
     public class JuegoDados: Juego
     {
+		private int cantMesas = 0;
+		
+		public override Mesa CrearMesa()
+		{
+			cantMesas++;
+			Mesa nuevaMesa = new MesaDados(cantMesas);
+			mesas.add(nuevaMesa);
+			return nuevaMesa;
+		}
+		
+		public override void Reset()
+		{
+			mesas.Clear();
+			cantMesas = 0;
+		}
     }
 }
