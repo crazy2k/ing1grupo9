d86361e508e0e7512a2a3b24c2ac1d0abe966c42
martaponzoni <martaponzoni@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-08-02 23:17:31 +0000

    mesa actualizada

--- Casino/CasinoNEW/Mesa.cs
+++ Casino/CasinoNEW/Mesa.cs
@@ -5,7 +5,7 @@ namespace CasinoNEW
 {
     public abstract class Mesa
     {
-        protected int id; //sacarlo
+        protected int id;
 		
 		public int Id {
 			get { return id; }
@@ -15,5 +15,6 @@ namespace CasinoNEW
         public abstract void jugar();
         public abstract void agregarParticipante(Jugador j);
         public abstract void quitarParticipante(Jugador j);
+        public abstract void apostar(Jugador j, Apuesta a);
     }
 }
