a36692d1d71a0300b39f5a1d9645d321e357e223
martaponzoni <martaponzoni@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-08-03 21:22:13 +0000

    eventhandler actualizado

--- Casino/CasinoNEW/EventHandlerDados.cs
+++ Casino/CasinoNEW/EventHandlerDados.cs
@@ -5,6 +5,8 @@ namespace CasinoNEW
 {
     public class EventHandlerDados
     {
+        public void agregarObservador(Jugador j) { }
+        public void quitarObservador(Jugador j) { }
         public void notificar(ReporteDados rep) { }
     }
 }
