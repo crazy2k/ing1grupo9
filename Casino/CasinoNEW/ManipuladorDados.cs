110852bb313c4e7fabde0649ced55692ffe9aa0d
martaponzoni <martaponzoni@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-08-03 01:06:04 +0000

    manipuladordados

---
+++ Casino/CasinoNEW/ManipuladorDados.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CasinoNEW
+{
+	class ManipuladorDados : Manipulador
+	{
+
+        #region Manipulador Members
+
+        public void manipularResultado(Resultado res)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public void manipularJugadaTP(List<Mesa> mesas)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public void manipularJugadaFeliz(Mesa mesa)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        #endregion
+    }
+}
