08efd75d909dce748ff89568e3d705224227277a
martaponzoni <martaponzoni@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-08-03 22:07:05 +0000

    agregue constructor

--- trunk/Casino/CasinoNEW/ResultadoDados.cs
+++ trunk/Casino/CasinoNEW/ResultadoDados.cs
@@ -7,7 +7,12 @@ namespace CasinoNEW
     {
         private int Dado1;
         private int Dado2;
-    
+
+    public ResultadoDados(int d1, int d2)
+        {
+            Dado1 = d1;
+            Dado2 = d2;
+        }
     public int sumaDados()
         {
             return Dado1+Dado2;
