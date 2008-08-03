2e4925f657f5ddcd8e5947b2a8a73d6695ee61d1
elmaildematiaz@gmail.com <elmaildematiaz@gmail.com@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-08-03 01:53:51 +0000

    * CrupierDados.cs: crupier

--- Casino/CasinoNEW/CrupierDados.cs
+++ Casino/CasinoNEW/CrupierDados.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Configuration;
 using System.Collections.Generic;
+using Dinero = System.Decimal;
 
 namespace CasinoNEW
 {
@@ -14,23 +15,52 @@ namespace CasinoNEW
             set { tirador = value; }
         }
 
-        private Dictionary<Jugador,List<Apuesta>> 
-            apuestasRealizadas = new Dictionary<Jugador,List<Apuesta>>();
-        private Dictionary<Jugador, List<Apuesta>>
-            apuestasPagadas = new Dictionary<Jugador, List<Apuesta>>();
-        private bool esTiroSalida;
+        private Dictionary<Jugador,List<ApuestaDados>> 
+            apuestasRealizadas = new Dictionary<Jugador,List<ApuestaDados>>();
+
+		private Dictionary<Jugador, List<ApuestaDados>>
+            apuestasPagadas = new Dictionary<Jugador, List<ApuestaDados>>();
+        
+		private bool esTiroSalida;
         private int punto;
         private ResultadoDados ultimoResultado;
         public void elegirTirador() { }
-        public void agregarApuesta(Jugador j,Apuesta a)
+        
+		public void agregarApuesta(Jugador j,Apuesta a)
         {
-            apuestasRealizadas[j].Add(a);
+			//Hay que ver si es de Dados
+//            apuestasRealizadas[j].Add(a);
         }
-        public void quitarApuesta(Jugador j, Apuesta a)
+
+		public void quitarApuesta(Jugador j, ApuestaDados a)
         {
             apuestasRealizadas[j].Remove(a);
         }
-        public void pagarApuestas(ResultadoDados res, TipoJugada tipo) { }
+		
+        public void pagarApuestas(ResultadoDados res, TipoJugada tipo) { 
+			foreach (Jugador j in apuestasRealizadas.Keys){
+				List<ApuestaDados> aps = apuestasRealizadas[j];
+				foreach (ApuestaDados a in aps){
+					Pair result = a.evaluar(res);
+					bool definida = result.getFirst();
+					Dinero valor = result.getSecond();
+					if (definida) {
+						if (tipo == TodosPonen )
+							valor = 
+			a.DescontarTodosPonen(Casino.GetInstance().PorcJugadaTodosPonen);
+						
+						Casino.GetInstance().Pagar(valor, j);
+						//Hay que sacarla de las apuestas a pagar y ponerla en
+						//las pagadas y fijarse que pasa cuando es feliz
+					}
+					
+					
+				
+				}
+			
+			}
+		
+		}
         public void borrarApuestasPagadas() { }
     }
 }
