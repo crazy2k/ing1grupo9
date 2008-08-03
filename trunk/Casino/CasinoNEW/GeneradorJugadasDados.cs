e7134b96b07c72abe91ac12a9fd023004c457d81
martaponzoni <martaponzoni@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-08-03 22:07:41 +0000

    generadorJugadasDados implementado completamente

--- trunk/Casino/CasinoNEW/GeneradorJugadasDados.cs
+++ trunk/Casino/CasinoNEW/GeneradorJugadasDados.cs
@@ -9,7 +9,44 @@ namespace CasinoNEW
 
         public Pair obtenerJugada()
         {
+            setTipoJugada(Casino.GetInstance().ObtenerTipoDeJugada());
+            Random r1 = new Random(DateTime.Now.Millisecond);
+            Random r2 = new Random(DateTime.Now.Millisecond);
+            double rFloat1 = r1.NextDouble();
+            double rFloat2 = r2.NextDouble();
+            int dado1=0, dado2=0;
+            setValorDado(dado1, rFloat1);
+            setValorDado(dado2, rFloat2);
+            setResultado(new ResultadoDados(dado1, dado2));
             return proxJugada;
+        }
+
+        private void setValorDado(int dado, double rFloat)
+        {
+            if (rFloat < 1 / 6)
+            {
+                dado = 1;
+            }
+            else if (rFloat >= 1 / 6 && rFloat < 2 / 6)
+            {
+                dado = 2;
+            }
+            else if (rFloat >= 2 / 6 && rFloat < 3 / 6)
+            {
+                dado = 3;
+            }
+            else if (rFloat >= 3 / 6 && rFloat < 4 / 6)
+            {
+                dado = 4;
+            }
+            else if (rFloat >= 4 / 6 && rFloat < 5 / 6)
+            {
+                dado = 5;
+            }
+            else
+            {
+                dado = 6;
+            }
         }
         public void setTipoJugada(TipoJugada t)
         {
