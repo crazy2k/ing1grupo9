1cff1572c62297c83921bbf73b7361e9978c256d
martaponzoni <martaponzoni@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-08-03 00:08:48 +0000

    implementacion de algunos metodos de mesadados

--- trunk/Casino/CasinoNEW/MesaDados.cs
+++ trunk/Casino/CasinoNEW/MesaDados.cs
@@ -1,17 +1,73 @@
 using System;
 using System.Configuration;
+using Jugada = CasinoNEW.Pair;
 
 namespace CasinoNEW
 {
     public class MesaDados : Mesa
     {
+        private AnfitrionDados anfitrion = new AnfitrionDados();
+
+        public AnfitrionDados Anfitrion
+        {
+			get { return anfitrion; }
+			set { anfitrion = value; }
+		}
+
+        private GeneradorJugadasDados generadorJugadas = new GeneradorJugadasDados();
+
+        public GeneradorJugadasDados GeneradorJugadas
+        {
+            get { return generadorJugadas; }
+            set { generadorJugadas = value; }
+        }
+
+        private EventHandlerDados eventHandler = new EventHandlerDados();
+
+        public EventHandlerDados EventHandler
+        {
+            get { return eventHandler; }
+            set { eventHandler = value; }
+        }
+
+        private ReportadorDados reportador = new ReportadorDados();
+
+        public ReportadorDados Reportador
+        {
+            get { return reportador; }
+            set { reportador = value; }
+        }
+
+        private CrupierDados crupier = new CrupierDados();
+
+        public CrupierDados Crupier
+        {
+            get { return crupier; }
+            set { crupier = value; }
+        }
+
 		public MesaDados(int identificador)
 		{
 			id = identificador;
 		}
-        public override void jugar() { }
-        public override void agregarParticipante(Jugador j) { }
-        public override void quitarParticipante(Jugador j) { }
-        public void reportar() { }
+        public override void jugar() 
+        {
+            Jugada jugada = GeneradorJugadas.obtenerJugada();
+            //falta
+        }
+        public override void apostar(Jugador j, Apuesta a) 
+        {
+            j.agregarApuesta(a);
+            Crupier.agregarApuesta(j, a);
+        }
+        public override void agregarParticipante(Jugador j) 
+        {
+            Anfitrion.recibirParticipante(j);
+        }
+        public override void quitarParticipante(Jugador j) 
+        {
+            Anfitrion.despedirParticipante(j);
+        }
+        public void notificarEstado() { }
     }
 }
