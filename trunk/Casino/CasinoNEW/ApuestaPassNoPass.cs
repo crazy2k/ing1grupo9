0ce493ba9fcf4dc0005730bfad44708069b7c1a3
martaponzoni <martaponzoni@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-08-04 04:45:32 +0000

    apuestapassnopass arreglada

--- trunk/Casino/CasinoNEW/ApuestaPassNoPass.cs
+++ trunk/Casino/CasinoNEW/ApuestaPassNoPass.cs
@@ -18,25 +18,29 @@ namespace CasinoNEW
                 if (res.sumaDados() == 7 || res.sumaDados() == 11) 
                 { 
                     premio = aFavor ? valor*2 : 0;
-                    tiroDeSalida = false; 
+                    tiroDeSalida = false;
+                    this.premio.MontoPremioJugada = premio;
                     return new Pair(true,premio); 
                 }
                 else if (res.sumaDados() == 2 || res.sumaDados() == 3) 
                 { 
                     premio = aFavor ? 0 : valor*2;
-                    tiroDeSalida = false; 
+                    tiroDeSalida = false;
+                    this.premio.MontoPremioJugada = premio;
                     return new Pair(true,premio); 
                 }
                 else if (res.sumaDados() == 12) 
                 { 
                     premio = aFavor ? 0 : valor;
-                    tiroDeSalida = false; 
+                    tiroDeSalida = false;
+                    this.premio.MontoPremioJugada = premio;
                     return new Pair(true,premio); 
                 } 
                 else 
                 {
                     punto = res.sumaDados();
-                    tiroDeSalida = false; 
+                    tiroDeSalida = false;
+                    this.premio.MontoPremioJugada = -1;
                     return new Pair(false,-1); 
                 } 
             }
@@ -45,15 +49,18 @@ namespace CasinoNEW
                 if (res.sumaDados() == 7) 
                 { 
                     premio = aFavor ? 0 : valor*2;
+                    this.premio.MontoPremioJugada = premio;
                     return new Pair(true,premio); 
                 }
                 else if (res.sumaDados() == punto) 
                 { 
                     premio = aFavor ? valor*2 : 0;
+                    this.premio.MontoPremioJugada = premio;
                     return new Pair(true, premio); 
                 } 
                 else 
-                { 
+                {
+                    this.premio.MontoPremioJugada = -1;
                     return new Pair(false,-1); 
                 }
             }
