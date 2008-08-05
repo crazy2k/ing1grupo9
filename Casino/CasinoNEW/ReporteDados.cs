6d7f76847ffe6e1670edb75404af8220179f3118
elmaildematiaz@gmail.com <elmaildematiaz@gmail.com@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-08-05 22:17:00 +0000

    * ReporteDados.cs: Resporte Dados definido

--- Casino/CasinoNEW/ReporteDados.cs
+++ Casino/CasinoNEW/ReporteDados.cs
@@ -6,6 +6,51 @@ namespace CasinoNEW
 {
     public class ReporteDados
     {
-
+		private List<Jugador> jugadores;
+		private Jugador proxTirador;
+		private bool proxTiroSalida;
+		private Resultado resultadoUltimo;
+		private Jugador ultimoTirador;
+		private List<Premio> premios;
+		private List<Apuesta> apuestas;
+		private	int punto;
+		private	int idMesa;
+				
+		public List<Jugador> Jugadores{
+			get{ return jugadores;}
+			set{ jugadores = value;}
+		}
+		public Jugador ProxTirador{
+			get{ return proxTirador;}
+			set{ proxTirador = value;}
+		}
+		public bool ProxTiroSalida{
+			get{ return proxTiroSalida;}
+			set{ proxTiroSalida = value;}
+		}
+		public Resultado ResultadoUltimo{
+			get{ return resultadoUltimo;}
+			set{ resultadoUltimo = value;}
+		}
+		public Jugador UltimoTirador{
+			get{ return ultimoTirador;}
+			set{ ultimoTirador = value;}
+		}
+		public List<Premio> Premios{
+			get{ return premios;}
+			set{ premios = value;}
+		}
+		public List<Apuesta> Apuestas{
+			get{ return apuestas;}
+			set{ apuestas = value;}
+		}
+		public	int Punto{
+			get{ return punto;}
+			set{ punto = value;}
+		}
+		public	int IdMesa{
+			get{ return idMesa;}
+			set{ idMesa = value;}
+		}
     }
 }
