6bcc07e117d16650743ad0acc93c3bc1ad109f86
elmaildematiaz@gmail.com <elmaildematiaz@gmail.com@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-08-06 15:52:54 +0000

    * ManejadorDados.cs: FALTA EL MANEJADOR ENTERO!!!!! * Terminal.cs: ahora sí, done el notificar de la terminal y del event   handler * ReporteDados.cs, CrupierDados.cs: cambio de List a IList

---
+++ trunk/Casino/CasinoNEW/ManejadorDados.cs
@@ -0,0 +1,29 @@
+// ManejadorDados.cs created with MonoDevelop
+// User: tas at 12:35 06/08/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+using System.Collections.Generic;
+
+namespace CasinoNEW
+{
+	
+	
+	public class ManejadorDados
+	{
+		public static void Notificar(int id, string usuario, int idMesa, 
+		                      IList<Jugador> jugadores, Jugador proxTirador,
+		                      bool proxTiroSalida, int punto, 
+		                      Jugador ultimoTirador, Resultado ultimoResultado, 
+		                      IList<Premio> premios, 
+		                     Dictionary<Jugador,IList<ApuestaDados>> apuestas){}
+		
+		//FALTA EL MANEJADOR ENTERO!!!!!!
+		
+		public ManejadorDados()
+		{
+		}
+	}
+}
