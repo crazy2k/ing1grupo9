921d1268f40f485b5a7598a853b252da048535de
elmaildematiaz@gmail.com <elmaildematiaz@gmail.com@438cbb89-2953-0410-b3f5-8f14b411ad03>
2008-08-06 20:49:05 +0000

    * ManejadorDados.cs: EntrarCraps a mesa existente y a nueva mesa

--- trunk/Casino/CasinoNEW/ManejadorDados.cs
+++ trunk/Casino/CasinoNEW/ManejadorDados.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Collections.Generic;
+using Dinero = System.Decimal;
 
 namespace CasinoNEW
 {
@@ -20,7 +21,34 @@ namespace CasinoNEW
 		                      IList<Premio> premios, 
 		                     Dictionary<Jugador,IList<ApuestaDados>> apuestas){}
 		
-		//FALTA EL MANEJADOR ENTERO!!!!!!
+	
+		public void EntrarCrapsMesa(int id, string usuario, int idMesa){
+			Jugador j = GestionadorUsuarios.GetInstance().GetJugador(usuario);
+			Mesa m = JuegoDados.GetInstance().getMesa(idMesa);
+			if ( !(m is MesaDados))
+				throw new Exception("El juego de dados tiene mesas de NoDados");
+//			EscritorDados escr = EscritorDados.GetInstance();
+			try{
+				j.entrarAMesa(m);
+//				escr.AceptarEntrada(id,usuario,idMesa, "");
+			}
+			catch (Exception e){
+//				escr.DenegarEntrada(id, usuario, idMesa, e.Message);
+			}
+		}
+		public void EntrarCrapsNuevaMesa(int id, string usuario){
+			Mesa m = JuegoDados.GetInstance().CrearMesa();
+			if ( !(m is MesaDados))
+				throw new Exception("El juego de dados tiene mesas de NoDados");
+			EntrarCrapsMesa(id, usuario , m.Id);
+		}
+		public void SalirCraps(int id, string usuario, int idMesa){}
+		public void Apostar(int id, string usuario, int idMesa, 
+		               TipoApuestaDados tap, int puntajeApostado, 
+		               Dinero valorFichas, int cantidadFichas){}
+		private void ApostarCraps(Jugador j, Mesa m, TipoApuestaDados tap, int cant,
+		                     Dinero ficha){}
+		public void Tirar(int id, string usuario, int idMesa){}
 		
 		public ManejadorDados()
 		{
